a412d1922679fd0b430a15f4e6b5047b36b6a410
155TuT <zhiyiwang155TuT@gmail.com>
2026-06-20 02:31:00 +0800

    fix: change LF to CRLF

--- homework/chap9_PWM_1/Debug/User_Component/LED_Pwm/subdir_rules.mk
+++ homework/chap9_PWM_1/Debug/User_Component/LED_Pwm/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 User_Component/LED_Pwm/%.obj: ../User_Component/LED_Pwm/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: C2000 Compiler'
-	"E:/ti/ccs930/ccs/tools/compiler/ti-cgt-c2000_18.12.4.LTS/bin/cl2000" -v28 -ml -mt --include_path="E:/ti/ccs930/ccs/tools/compiler/ti-cgt-c2000_18.12.4.LTS/include" --include_path="E:/ti/workspace_v9/chap9_PWM_1" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="User_Component/LED_Pwm/$(basename $(<F)).d_raw" --obj_directory="User_Component/LED_Pwm" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/bin/cl2000" -v28 -ml -mt --include_path="C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include" --include_path="C:/Users/17169/Documents/Mycode/fzucourses/Electivecourse/2502-fzu-SingleChip-C2000-WuWang/homework/chap9_PWM_1" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="User_Component/LED_Pwm/$(basename $(<F)).d_raw" --obj_directory="User_Component/LED_Pwm" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
--- homework/chap9_PWM_1/Release/F2802x_Component/subdir_rules.mk
+++ homework/chap9_PWM_1/Release/F2802x_Component/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 F2802x_Component/%.obj: ../F2802x_Component/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: C2000 Compiler'
-	"E:/ti/ccs930/ccs/tools/compiler/ti-cgt-c2000_18.12.4.LTS/bin/cl2000" -v28 -ml -mt -O2 --include_path="E:/ti/ccs930/ccs/tools/compiler/ti-cgt-c2000_18.12.4.LTS/include" --include_path="E:/ti/workspace_v9/myproject6-pwm" --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="F2802x_Component/$(basename $(<F)).d_raw" --obj_directory="F2802x_Component" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/bin/cl2000" -v28 -ml -mt -O2 --include_path="C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include" --include_path="C:/Users/17169/Documents/Mycode/fzucourses/Electivecourse/2502-fzu-SingleChip-C2000-WuWang/homework/chap9_PWM_1" --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="F2802x_Component/$(basename $(<F)).d_raw" --obj_directory="F2802x_Component" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
